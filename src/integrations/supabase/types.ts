d59c76cee083283017b6c5119e5a73a37487bdc4
Webber-Lubenham <frank.webber.dev@gmail.com>
2025-04-30 12:12:02 +0100

    Atualiza tipo de user_id em locations de number para string|null no types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -69,21 +69,21 @@ export type Database = {
           latitude: number
           longitude: number
           timestamp: string
-          user_id: number | null
+          user_id: string | null
         }
         Insert: {
           id?: string
           latitude: number
           longitude: number
           timestamp?: string
-          user_id?: number | null
+          user_id?: string | null
         }
         Update: {
           id?: string
           latitude?: number
           longitude?: number
           timestamp?: string
-          user_id?: number | null
+          user_id?: string | null
         }
         Relationships: [
           {
